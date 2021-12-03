3b2634bd9ea485e34d12c43bf5883ba01864a7e8
etienne.raby <etienne.raby@fittingbox.com>
2021-12-03 20:31:41 +0100

    init with tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.svelte'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
