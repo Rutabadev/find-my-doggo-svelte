557530ac24abda4e68a6226a716497edd0e0da8f
Rutabadev <etienner37@gmail.com>
2022-01-21 00:10:03 +0100

    no outline dropdown focus

--- src/lib/utils/Dropdown.svelte
+++ src/lib/utils/Dropdown.svelte
@@ -86,7 +86,7 @@
 						item.click?.();
 						menuShown = false;
 					}}
-					class="w-full px-4 py-2 text-sm text-gray-700 dark:text-gray-100 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-gray-600 dark:focus:bg-gray-600 cursor-pointer"
+					class="w-full px-4 py-2 text-sm text-gray-700 dark:text-gray-100 hover:bg-gray-200 focus:bg-gray-200 dark:hover:bg-gray-600 dark:focus:bg-gray-600 cursor-pointer outline-none"
 					>{item.content}
 				</button>
 			{/each}
