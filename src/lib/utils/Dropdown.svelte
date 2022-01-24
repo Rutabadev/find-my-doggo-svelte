59a01c16977292cd5ff6a5defed4687935d99c59
Etienne RABY <etienne.raby@fittingbox.com>
2022-01-24 14:13:07 +0100

    clean refacto

--- src/lib/utils/Dropdown.svelte
+++ src/lib/utils/Dropdown.svelte
@@ -11,44 +11,23 @@
 
 	let dropdownElement: HTMLElement;
 	let itemsElement: HTMLElement;
+	$: itemsList = itemsElement && Array.from(itemsElement.children);
 	let selectedItemElement: HTMLElement | undefined;
 	let menuShown = false;
 	$: if (browser) {
 		// Run only client side to access window
 		if (menuShown) {
-			window.addEventListener('click', clickOutsideHandler);
+			window.addEventListener('click', clickOutsideClose);
 			window.addEventListener('keydown', keydownHandler);
 		} else {
-			window.removeEventListener('click', clickOutsideHandler);
+			window.removeEventListener('click', clickOutsideClose);
 			window.removeEventListener('keydown', keydownHandler);
 			selectedItemElement = undefined;
 		}
 	}
 
-	function selectElement(position: 'next' | 'previous') {
-		const [firstItem, nextItem] =
-			position === 'next'
-				? ['firstElementChild', 'nextElementSibling']
-				: ['lastElementChild', 'previousElementSibling'];
-		selectedItemElement = !selectedItemElement
-			? itemsElement[firstItem]
-			: selectedItemElement[nextItem] || itemsElement[firstItem];
-
-		selectedItemElement.focus();
-	}
-
-	function clickOutsideHandler({ target }: MouseEvent) {
+	function clickOutsideClose({ target }: MouseEvent) {
 		if (!dropdownElement.contains(target as Node)) menuShown = false;
-	}
-
-	function searchItem(key: string, startSearchAt = 0) {
-		const itemsList = Array.from(itemsElement.children);
-		return (itemsList
-			.slice(startSearchAt, itemsList.length)
-			.find(({ textContent }) => textContent.startsWith(key)) ||
-			itemsList
-				.slice(0, itemsList.length)
-				.find(({ textContent }) => textContent.startsWith(key))) as HTMLElement;
 	}
 
 	function keydownHandler(e) {
@@ -76,10 +55,8 @@
 					return;
 				}
 
-				const itemFound = searchItem(
-					key,
-					Array.from(itemsElement.children).indexOf(selectedItemElement) + 1
-				);
+				const itemFound =
+					searchItem(key, itemsList.indexOf(selectedItemElement) + 1) || searchItem(key);
 
 				if (!itemFound) {
 					return;
@@ -88,6 +65,24 @@
 				selectedItemElement = itemFound;
 				selectedItemElement.focus();
 		}
+	}
+
+	function selectElement(position: 'next' | 'previous') {
+		const [firstItem, nextItem] =
+			position === 'next'
+				? ['firstElementChild', 'nextElementSibling']
+				: ['lastElementChild', 'previousElementSibling'];
+		selectedItemElement = !selectedItemElement
+			? itemsElement[firstItem]
+			: selectedItemElement[nextItem] || itemsElement[firstItem];
+
+		selectedItemElement.focus();
+	}
+
+	function searchItem(key: string, startSearchAt = 0) {
+		return itemsList
+			.slice(startSearchAt, itemsList.length)
+			.find(({ textContent }) => textContent.startsWith(key)) as HTMLElement;
 	}
 
 	function scaleY(node, { duration = 150, start = 0 }) {
@@ -103,10 +98,10 @@
 	class="
 		{$$props.class}
 		relative inline-flex items-center rounded-md py-2 px-4 border border-gray-300 dark:border-gray-700
-		{!$$props.class.split(' ').some((name) => name.match(/^(.+:)?bg-\w+-\d+$/)) &&
+		{!$$props.class?.split(' ').some((name) => name.match(/^(.+:)?bg-\w+-\d+$/)) &&
 		'bg-gray-50 dark:bg-gray-800'}
 		text-sm font-medium shadow
-		{!$$props.class.split(' ').some((name) => name.match(/^(.+:)?text-\w+-\d+$/)) &&
+		{!$$props.class?.split(' ').some((name) => name.match(/^(.+:)?text-\w+-\d+$/)) &&
 		'text-gray-700 dark:text-gray-100'}
 		"
 	on:click={() => (menuShown = !menuShown)}
